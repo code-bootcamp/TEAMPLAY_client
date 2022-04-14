5aa8b005113869973c3a0157cd22b8e3ad2494a3
godboy4256 <godboy4256@github.com>
2022-04-14 21:59:47 +0900

    확인용

--- src/components/commons/searchbar/searchbar.query.ts
+++ src/components/commons/searchbar/searchbar.query.ts
@@ -1,1 +1,7 @@
+import { gql } from '@apollo/client';
 
+export const DELETE_PROJECT = gql`
+   mutation deleteProject($projectId:String!){
+    deleteProject(projectId:$projectId)
+   }
+`
--- src/components/commons/searchbar/searchbar.types.ts
+++ src/components/commons/searchbar/searchbar.types.ts
@@ -1,1 +1,3 @@
-
+export interface IPropsProjectManage {
+    project?:string | string[] | undefined;
+}
